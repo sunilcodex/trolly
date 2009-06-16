2c352c4c66e980c1ec13a118c936ff6cf63bc7a1
benny.caldwell <benny.caldwell@278c77e0-5a78-11de-8d7d-01d876f1962d>
2009-06-16 13:39:02 +0000

    Created Trolly android project.

---
+++ src/captainfanatic/trolly/Trolly.java
@@ -0,0 +1,13 @@
+package captainfanatic.trolly;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class Trolly extends ListActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
